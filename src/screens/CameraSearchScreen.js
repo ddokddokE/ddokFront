351d8e4905be4780a34603ba1f3e1d5019d49994
JE <seoje105@gmail.com>
2024-06-09 15:11:55 +0900

    api

--- src/screens/CameraSearchScreen.js
+++ src/screens/CameraSearchScreen.js
@@ -1,19 +1,72 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 import AppHeader from '../components/HeaderComponent';
 import useSpeakOnFocus from '../utils/DescriptionUtil';
+
+import { CameraScreen } from 'react-native-camera-kit';
 
 const CameraSearchScreen = ({ navigation }) => {
 
   useSpeakOnFocus('카메라로 의약품을 검색하는 화면입니다.');
 
+  const onBarcodeScan = (qrvalue) => {
+    // Called after te successful scanning of QRCode/Barcode
+    setQrvalue(qrvalue);
+    setOpneScanner(false);
+  };
+
+  const onOpneScanner = () => {
+    // To Start Scanning
+    if (Platform.OS === 'android') {
+      async function requestCameraPermission() {
+        try {
+          const granted = await PermissionsAndroid.request(
+            PermissionsAndroid.PERMISSIONS.CAMERA,
+            {
+              title: 'Camera Permission',
+              message: 'App needs permission for camera access',
+            },
+          );
+          if (granted === PermissionsAndroid.RESULTS.GRANTED) {
+            // If CAMERA Permission is granted
+            setQrvalue('');
+            setOpneScanner(true);
+          } else {
+            alert('CAMERA permission denied');
+          }
+        } catch (err) {
+          alert('Camera permission err', err);
+          console.warn(err);
+        }
+      }
+      // Calling the camera permission function
+      requestCameraPermission();
+    } else {
+      setQrvalue('');
+      setOpneScanner(true);
+    }
+  };
 
   return (
     <View style={styles.container}>
       <AppHeader navigation={navigation} />
       <Text style={styles.description}>카메라로 의약품을 검색하는 화면입니다.</Text>
       <View style={styles.content}>
-        <Text>CameraSearch Screen</Text>
+        <CameraScreen
+            showFrame={false}
+            // Show/hide scan frame
+            scanBarcode={true}
+            // Can restrict for the QR Code only
+            laserColor={'blue'}
+            // Color can be of your choice
+            frameColor={'yellow'}
+            // If frame is visible then frame color
+            colorForScannerFrame={'black'}
+            // Scanner Frame color
+            onReadCode={(event) =>
+              onBarcodeScan(event.nativeEvent.codeStringValue)
+            }
+          />
       </View>
     </View>
   );
