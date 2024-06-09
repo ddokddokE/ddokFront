874771de9799aa715d6d2bef4de00d391d64a0a4
JE <seoje105@gmail.com>
2024-06-09 11:32:49 +0900

    설정파일 및 app.tsx

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,11 @@
 module.exports = {
   root: true,
-  extends: '@react-native',
+  extends: [
+    '@react-native',
+    '@react-native-community',
+    'prettier'
+  ],
+  rules: {
+    'prettier/prettier': ['error', { 'endOfLine': 'auto' }],
+  },
 };
